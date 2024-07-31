15358a36207b1a6f7c0a743b052a5b3ed33db172
Joshua Dadula <jozhuare99@gmail.com>
2024-07-31 16:50:49 +0800

    add  some script basic

--- index.js
+++ index.js
@@ -33,7 +33,8 @@ app.post('/upload', (req, res) => {
     }
   });
 });
+const port = "3000"
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server started on port 3000');
 });
